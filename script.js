0e3a93642981f29b5253d95ceec016b86e71b6a9
Максим Огольцов <ogoltsov2004@mail.ru>
2025-01-17 13:18:51 +0300

    Update

--- script.js
+++ script.js
@@ -2,26 +2,41 @@ const button = document.getElementById('add-task-btn');
 const inputText = document.getElementById('MyInput');
 const Tasks = document.getElementById('currentTask');
 
+///Функция вывода данных из LocalStorage
  function GetData(){
+    /// If для проверки есть ли данные
     if (localStorage.getItem("Data")){
   Tasks.innerHTML = localStorage.getItem("Data");
+  RestoreTask();
   }
  }
 
+ function RestoreTask(){
+    const taskItems = Tasks.querySelectorAll('li');
+    taskItems.forEach((taskItem) =>{
+        AddButtonsToTask(taskItem);
+    })
+ }
+
+
+
+///Функция сохранения данных
  function SaveData(){
     localStorage.setItem("Data",Tasks.innerHTML)
  }
 
  GetData();
-
+localStorage.clear();
 
 button.addEventListener('click', function(){
     const TaskText = inputText.value;
-
     if (TaskText.trim() !== ''){
         const NewTask = document.createElement('li');
         NewTask.textContent = TaskText;
+        NewTask.classList.add('SelectedStyleTask');
+        AddButtonsToTask(NewTask);
         Tasks.appendChild(NewTask);
+        ///была проблема что написал функции, но не вызвал
         SaveData();
         inputText.value='';
         
@@ -30,4 +45,27 @@ button.addEventListener('click', function(){
 }
 });
 
+function AddButtonsToTask(taskItem) {
+    if (!taskItem.querySelector('.Delete-button')) {
+        const DeleteButton = document.createElement('button');
+        DeleteButton.textContent = "x";
+        DeleteButton.classList.add('Delete-button');
+        DeleteButton.addEventListener('click', () => {
+            console.log("Удалено");
+            taskItem.remove();
+            SaveData();
+        });
+
+        taskItem.appendChild(DeleteButton); 
+    }
+}
+
+
+Tasks.addEventListener('click', function (event) {
+    const SelectedTask = event.target; // Текущий элемент, на который кликнули
+    if (SelectedTask.classList.contains('SelectedStyleTask') && !SelectedTask.classList.contains('Delete-button')) {
+        SelectedTask.classList.toggle('completed'); // Добавляем/удаляем зачеркивание
+        SaveData();
+    }
+});
 
